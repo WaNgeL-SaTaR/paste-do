43a26b97f33168cfe549382072f395609bd44e4e
WaNgeL-SaTaR <tapacpro@gmail.com>
2015-08-24 22:20:52 +0500

    work with requests

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,14 +1,32 @@
-// Init editor
 (function(){
-  var simplemde = new SimpleMDE({ element: document.getElementById('paste-form') });
+  // Init editor
+  var simplemde = new SimpleMDE({
+    element: document.getElementById('paste-form'),
+    status: true,
+    status: ['autosave', 'lines', 'words', 'cursor'], // Optional usage
+    toolbar: true,
+    toolbarTips: true,
+    toolbarGuideIcon: false,
+    autofocus: true,
+    lineWrapping: true,
+    indentWithTabs: false,
+    tabSize: 2,
+    spellChecker: false,
+    autosave: {
+        enabled: true,
+        unique_id: 'paste-form-save',
+        delay: 10000,
+    },
+  });
   simplemde.render();
-  // Listen click
-  document.getElementById('paste-save').addEventListener('click', function() {
-    // создать объект для формы
-    var formData = new FormData(simplemde.value());
-    // отослать
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "/new/");
-    xhr.send(formData);
-  });  
+
+  // // Listen click
+  // document.getElementById('paste-save').addEventListener('click', function() {
+  //   var formData = new FormData(simplemde.value());
+  //   // Send
+  //   var xhr = new XMLHttpRequest();
+  //   xhr.open("POST", "/new/");
+  //   xhr.send(formData);
+  // });
+
 })();
